2f500ea62463c77b0b5674a2f54f8b8f8436daad
sounishnath003 <flock.sinasini@gmail.com>
2019-09-01 07:21:22 +0530

    BottomAppBar

--- lib/aAppHome.dart
+++ lib/aAppHome.dart
@@ -7,21 +7,55 @@ class AppHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-        title: new Text(" Kolkata Guide "),
-        centerTitle: true,
-        elevation: 0.0,
-        actions: <Widget>[
-          Icon(Icons.account_circle, size: 30,),
-          SizedBox(width: 10,)
-        ],
-      ),
-      body: AppBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Icon(Icons.add),
-        backgroundColor: Colors.black,
-      ),
-    );
+        appBar: new AppBar(
+          title: new Text(" Kolkata Guide "),
+          centerTitle: true,
+          elevation: 0.0,
+          actions: <Widget>[
+            Icon(
+              Icons.account_circle,
+              size: 30,
+            ),
+            SizedBox(
+              width: 10,
+            )
+          ],
+        ),
+        body: AppBody(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: _floatingActionButton(),
+        bottomNavigationBar: _bottomAppBar());
   }
+}
+
+Widget _floatingActionButton() {
+  return FloatingActionButton(
+    onPressed: () {},
+    child: Icon(Icons.add),
+    backgroundColor: Colors.black,
+    // elevation: 0.0,
+  );
+}
+
+Widget _bottomAppBar() {
+  return BottomAppBar(
+    shape: CircularNotchedRectangle(),
+    notchMargin: 4.0,
+    child: Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        _iconBottomBar(Icon(Icons.menu), 25),
+        _iconBottomBar(Icon(Icons.search), 25)
+      ],
+    ),
+  );
+}
+
+Widget _iconBottomBar(Icon icon, double size){
+  return IconButton(
+    onPressed: (){}, 
+    icon: icon,
+    iconSize: size,
+  );
 }
