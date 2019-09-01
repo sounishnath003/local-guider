43835c5a3e943295a91395727c6d432fb8ac4ea3
sounishnath003 <flock.sinasini@gmail.com>
2019-09-01 14:20:24 +0530

    Card stack

--- lib/MenuPage.dart
+++ lib/MenuPage.dart
@@ -38,61 +38,66 @@ class MenuPageBody extends StatefulWidget {
 }
 
 class _MenuPageBodyState extends State<MenuPageBody> {
+
+  List<TouristsSpots> touristSpots = touristsSpots;
+
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Stack(
-            children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).size.height * 0.42,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(colors: <Color>[
-                      Colors.redAccent,
-                      Colors.red,
-                    ]),
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(50),
-                        bottomRight: Radius.circular(50))),
-              ),
-              AppBar(
-                backgroundColor: Colors.transparent,
-                title: Text(
-                  "Tourists Spots",
-                  style: TextStyle(color: Colors.white, fontSize: 22),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.42,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: <Color>[
+                        Colors.redAccent,
+                        Colors.red,
+                      ]),
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(50),
+                          bottomRight: Radius.circular(50))),
                 ),
-                centerTitle: true,
-                elevation: 0.0,
-              ),
-              _positionBannerText(context),
-            ],
-          ),
-          sizebox(10),
-          Center(
-            child: Text(
-              "Sweetness of Rosogolla",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1,
+                AppBar(
+                  backgroundColor: Colors.transparent,
+                  title: Text(
+                    "Tourists Spots",
+                    style: TextStyle(color: Colors.white, fontSize: 22),
+                  ),
+                  centerTitle: true,
+                  elevation: 0.0,
+                ),
+                _positionBannerText(context),
+              ],
+            ),
+            sizebox(10),
+
+            Center(
+              child: Text(
+                "Sweetness of Rosogolla",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1,
+                ),
               ),
             ),
-          ),
-          sizebox(15),
-          Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text(
-                "Our story",
-                style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
-              )),
-          sizebox(10),
-          _touristsPlace(context)
-        ],
-      ),
+            sizebox(15),
+            Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(
+                  "Our story",
+                  style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+                )),
+            sizebox(10),
+            _touristsPlace(context)
+
+          ],
+        ),
     );
   }
 }
@@ -111,28 +116,36 @@ Widget _touristsPlace(BuildContext context) {
           return Wrap(
             direction: Axis.horizontal,
             children: <Widget>[
-              new Card(
-                child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage(touristSpots[index].imagePath),
-                        )
-                    ),
-                    child: new Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.max,
-                      children: <Widget>[
-                        Stack(
-                          children: <Widget>[
-                            
-                          ],
-                        )
-                      ],
+              Padding(
+                padding: const EdgeInsets.only(left: 15.0),
+                child: new Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Container(
+                      width: 220,
+                      height: 140,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(touristSpots[index].imagePath),
+                          )
+                      ),
+                      child: new Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Stack(
+                            children: <Widget>[
+                              Text(touristSpots[index].name,
+                              style: TextStyle(
+                                decorationColor: Colors.redAccent,
+                                fontWeight: FontWeight.bold
+                              ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
